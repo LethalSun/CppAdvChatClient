39390376bae9c4bf981b0d5867da18cb4c3b6cc6
DESKTOP-2PJHP3P\LittleHill <massivedynamicplay@gmail.com>
2017-04-16 15:19:15 +0900

    no message

--- chatClientNotTest/chatClient/ChatClient.cpp
+++ chatClientNotTest/chatClient/ChatClient.cpp
@@ -4,15 +4,43 @@
 
 ChatClient::ChatClient()
 	:loginWindow(GUIStyle::Default)
+	, channelWindow(GUIStyle::Default)
+	,mainWindow(GUIStyle::Default)
 	, font{30}
 	, font2{20}
 {
-	loginWindow.setTitle(L"name");
-	loginWindow.addln(L"mainText",GUIText::Create(L"Login"));
+	//init login window
+	loginWindow.setTitle(L"Login");
+	//loginWindow.addln(L"mainText",GUIText::Create(L"Login"));
 	loginWindow.addln(L"LoginId", GUITextField::Create(none));
 	loginWindow.addln(L"LoginPassword", GUITextField::Create(none));
 	loginWindow.addln(L"LoginButton", GUIButton::Create(L"Login"));
 	//loginWindow.setCenter(Window::Center());
+
+	//init  chennel window
+	channelWindow.setTitle(L"Channel");
+	channelWindow.setPos(Point(200, 300));
+	channelWindow.add(L"channel0", GUIText::Create(L"hello    "));
+	channelWindow.addln(L"channel0Button", GUIButton::Create(L"Enter"));
+	channelWindow.add(L"channel1", GUIText::Create(L"hell     "));
+	channelWindow.addln(L"channel0Button", GUIButton::Create(L"Enter"));
+	channelWindow.add(L"channel2", GUIText::Create(L"hello    "));
+	channelWindow.addln(L"channel0Button", GUIButton::Create(L"Enter"));
+	channelWindow.add(L"channel3", GUIText::Create(L"hi       "));
+	channelWindow.addln(L"channel0Button", GUIButton::Create(L"Enter"));
+	channelWindow.add(L"channel4", GUIText::Create(L"who?     "));
+	channelWindow.addln(L"channel0Button", GUIButton::Create(L"Enter"));
+	
+	//init main window
+
+	mainWindow.setTitle(L"Main");
+	mainWindow.setPos(Point{ 500,500 });
+	mainWindow.style.showBoxModel = true;
+	mainWindow.addln(L"testText", GUIText::Create(L"hello"));
+	mainWindow.addln(L"inputField", GUITextArea::Create(4,20));
+
+	mainWindow.textArea(L"inputField").style.padding.left = 50;
+	mainWindow.textArea(L"inputField").style.padding.right = 50;
 }
 
 
@@ -27,7 +55,11 @@ int ChatClient::Update()
 		loginID = loginWindow.textField(L"LoginId")._get_text();
 		loginPassWord = loginWindow.textField(L"LoginPassword")._get_text();
 	}
-	
+
+	chatInput = mainWindow.textArea(L"inputField")._get_text();
+
+	//font(chatInput).drawCenter(600,360);
+	//PutText(L"a:", chatInput);
 	//font(loginID).drawCenter(Window::Center());
 	//font2(loginPassWord).draw(Mouse::Pos());
 	return 0;
--- chatClientNotTest/chatClient/ChatClient.h
+++ chatClientNotTest/chatClient/ChatClient.h
@@ -8,14 +8,20 @@ public:
 	int Update();
 private:
 
-	//TODO: 2.스테이트 패턴을 사용해서 윈도우 간의 전환.
+	//TODO: 3.스테이트 패턴을 사용해서 윈도우 간의 전환.
+	//TODO: 신관리 라는것이 존재한다.https://github.com/Siv3D/Reference-JP/wiki/%E3%82%B7%E3%83%BC%E3%83%B3%E7%AE%A1%E7%90%86
 	GUI loginWindow;
-	//TODO: 1.우선 메인 윈도우 부터 구현
+	//TODO: 1.우선 메인 윈도우 부터
+	//구현 실제 체팅이 이루워지는 공간
 	GUI mainWindow;
+	
+	//TODO: 2.대화방 선택
+	//대화방 최대 갯수.
+	GUI channelWindow;
 
 	String loginID{};
 	String loginPassWord{};
-
+	String chatInput{};
 	//TODO:나중에 지울거
 	const Font font;
 	const Font font2;
